14f98a8dc47f9bed675118f5c53468fdc1b390d5
Aswin-PK <aswinpk7737@gmail.com>
2024-01-14 16:39:15 +0530

    Accordion component added with single and multi-selection opening

---
+++ basic-features/src/Components/Accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What is React.js?",
+        answer: "React is a free and open-source front-end JavaScript library for building user interfaces based on components."
+    },
+    {
+        id: '2',
+        question: "Why it is called React?",
+        answer: "It 'reacts' quickly to changes without reloading the whole page. It uses the virtual DOM to efficiently update parts of a webpage."
+    },
+    {
+        id: '3',
+        question: "What is the main concept of React?",
+        answer: "React allows us to pass information to a Component using something called props."
+    },
+    {
+        id: '4',
+        question: "Is React backend or frontend?",
+        answer: "React Js Js is a globally used Front-end JS framework and is popular with both software developers and project sponsors."
+    }
+]
+
+export default data;
---
+++ basic-features/src/Components/Accordion/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import data from './data'
+import './style.css'
+export default function Accordion() {
+
+    const [selection, setSelection] = useState(null);
+    const [multiselection, setMultiselection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+    
+    const handleSelection = (value) =>{
+        if (multiselection){
+            let cpySelections = [...multiple];
+            const currentItem = cpySelections.indexOf(value);
+
+            if(currentItem === -1) cpySelections.push(value)
+            else cpySelections.splice(currentItem, 1)
+
+            setMultiple(cpySelections)
+        }
+        else {
+            setSelection(selection === value ? null : value)
+        }
+    }
+    
+    return <div className="wrapper">
+        <button className="multi-selection" onClick={()=>setMultiselection(!multiselection)}>Select Multiple Items {multiselection && <span style={{color: 'green'}}>(on)</span>}</button>
+        {
+            data && data.length > 0 ? (
+                data.map((currentQuestion, index) => (
+                    <>
+                        <div className="accordion" key={index} onClick={()=>handleSelection(currentQuestion.id)}>
+                            <span>{currentQuestion.question}</span>
+                            <span>+</span>
+                        </div>
+                        {
+                            selection === currentQuestion.id || 
+                            multiple.indexOf(currentQuestion.id) !== -1 ?(
+                                <span className='answer'>{currentQuestion.answer}</span>
+                            ): null
+                        }
+                    </>
+                ))
+            )
+            : <span>No data found</span>
+        }
+    </div>
+}
